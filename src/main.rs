b1aba173e0b2e98bba4798ac6bd8dab4223f6485
Jujumba <far77578@gmail.com>
2023-10-08 12:10:21 +0300

    Fix concurrent jobs' distribution

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,8 @@ use chrono::NaiveDateTime;
 use clap::Parser;
 use serde::{Deserialize, Serialize};
 
+use rayon::{prelude::*, current_num_threads};
+
 use std::borrow::Cow;
 use std::collections::HashMap;
 use std::hash::Hash;
@@ -11,10 +13,14 @@ use std::{fs, io};
 fn main() -> io::Result<()> {
     let cli = Cli::parse();
 
+    if let Some(jobs) = cli.jobs {
+        rayon::ThreadPoolBuilder::new().num_threads(jobs).build_global().unwrap();
+    }
+
     let content = fs::read_to_string(cli.file)?;
 
     let chat: Chat = serde_json::from_str(&content).unwrap();
-    let stat: ChatStatistics = ChatStatistics::gather(&chat, cli.jobs);
+    let stat: ChatStatistics = ChatStatistics::gather(&chat);
 
     let file = fs::File::create(cli.output)?;
     serde_json::to_writer_pretty(file, &stat)?;
@@ -28,8 +34,8 @@ struct Cli {
     file: PathBuf,
     #[arg(long, short, default_value = "out.json")]
     output: PathBuf,
-    #[arg(long, short, default_value_t = 12)]
-    jobs: usize
+    #[arg(long, short)]
+    jobs: Option<usize>
 }
 
 #[derive(Debug, Serialize, Deserialize, Eq, PartialEq, Hash, Clone)]
@@ -89,7 +95,7 @@ enum MessageType {
 #[derive(Debug, Serialize, Deserialize)]
 struct TextEntity {
     #[serde(rename = "type")]
-    _type: TextEntityType,
+    text_type: TextEntityType,
     text: String,
 }
 
@@ -116,6 +122,13 @@ enum TextEntityType {
     Strikethrough,
 }
 
+impl TextEntityType {
+    pub fn is_meta(&self) -> bool {
+        use TextEntityType::*;
+        matches!(self, Phone | BotCommand | Email | CustomEmoji | Mention)
+    }
+}
+
 #[derive(Debug, Serialize, Deserialize)]
 struct ChatStatistics<'a> {
     num_tokens: usize,
@@ -125,58 +138,51 @@ struct ChatStatistics<'a> {
     tokens_map: HashMap<Token<'a>, usize>,
 }
 impl<'a> ChatStatistics<'a> {
-    fn gather(chat: &'a Chat, jobs: usize) -> Self {
-        let iter = chat.messages.iter().filter(|message| message.message_type != MessageType::Service);
+    pub fn gather(chat: &'a Chat) -> Self {
+        let jobs = current_num_threads();
 
-        let num_messages = iter.clone().count();
+        let num_messages = chat.messages.len();
         let messages_per_thread = num_messages / jobs;
-        let (sender, receiver) = std::sync::mpsc::sync_channel(jobs);
 
+        let (sender, receiver) = crossbeam::channel::bounded(jobs + 1);
         let mut tokens_map = HashMap::new();
         let mut members_tokens_map = HashMap::new();
 
-        std::thread::scope(|s| {
-            for i in 0..jobs {
-                let iter = iter.clone();
-                let sender = sender.clone();
-                s.spawn(move || {
-                    let mut chunk_tokens_map = HashMap::new();
-                    let mut chunk_members_tokens_map: HashMap<Person<'_>, HashMap<Token<'_>, usize>> = HashMap::new();
-
-                    for message in iter.skip(i * messages_per_thread).take(messages_per_thread) {
-                        let from = message.from.clone().unwrap(); // okay since we are not copying the actual data
-                        for entity in &message.text_entities {
-                            for token in entity.text.split([' ', ',', '.','(', ')', '-', '!', '?', '\'', '\"', '\n', '\t']).filter(|s| !s.is_empty()) {
-                                let token = Token::from(remove_emojis(token)); // <-- such a performance hit!
-                                *chunk_tokens_map.entry(token.clone()).or_insert(0) += 1;
-                                match chunk_members_tokens_map.get_mut(&from) {
-                                    Some(map) => *map.entry(token).or_insert(0) += 1,
-                                    None => {
-                                        let member_occurences_map = HashMap::from([(token, 1)]);
-                                        chunk_members_tokens_map.insert(from.clone(), member_occurences_map);
-                                    }
-                                }
-                            } 
+        chat.messages.par_iter().chunks(messages_per_thread).for_each_with(sender, move |sender, messages: Vec<&Message>| {
+            let mut chunk_tokens_map: HashMap<Token<'_>, usize> = HashMap::new();
+            let mut chunk_members_tokens_map: HashMap<Person<'_>, HashMap<Token<'_>, usize>> = HashMap::new();
+            for message in messages.iter().filter(|message| message.message_type != MessageType::Service) {
+                let from = message.from.clone().unwrap();
+                for entity in message.text_entities.iter().filter(|entity| !entity.text_type.is_meta()) {
+                    for token in entity.text.split([' ', ',', '.','(', ')', '-', '!', '?', '\'', '\"', '\n', '\t']).filter(|s| !s.is_empty()) {
+                        let token = Token::from(remove_emojis(token)); // <-- such a performance hit!
+                        *chunk_tokens_map.entry(token.clone()).or_insert(0) += 1;
+                        match chunk_members_tokens_map.get_mut(&from) {
+                            Some(map) => *map.entry(token).or_insert(0) += 1,
+                            None => {
+                                let member_occurences_map = HashMap::from([(token, 1)]);
+                                chunk_members_tokens_map.insert(from.clone(), member_occurences_map);
+                            }
                         }
-                    }
-                    sender.send((chunk_tokens_map, chunk_members_tokens_map)).unwrap();
-                });
-            }
-            for _ in 0..jobs {
-                let (chunk_tokens_map, chunk_members_tokens_map) = receiver.recv().unwrap();
-                merge_maps_with(&mut tokens_map, chunk_tokens_map, |tokens_map, token, occurences| *tokens_map.entry(token).or_insert(0) += occurences);
-                for (member, map) in chunk_members_tokens_map {
-                    match members_tokens_map.get_mut(&member) {
-                        None => {
-                            members_tokens_map.insert(member, map);
-                        },
-                        Some(mergee) => {
-                            merge_maps_with(mergee, map, |mergee, member, occurences| *mergee.entry(member).or_insert(0) += occurences);
-                        },
-                    }
+                    } 
                 }
             }
+            sender.send((chunk_tokens_map, chunk_members_tokens_map)).unwrap();
         });
+        for _ in 0..jobs {
+            let (chunk_tokens_map, chunk_members_tokens_map): (HashMap<Token<'_>, usize>, _) = receiver.recv().unwrap();
+            merge_maps_with(&mut tokens_map, chunk_tokens_map, |tokens_map, token, occurences| *tokens_map.entry(token).or_insert(0) += occurences);
+            for (member, map) in chunk_members_tokens_map {
+                match members_tokens_map.get_mut(&member) {
+                    None => {
+                        members_tokens_map.insert(member, map);
+                    },
+                    Some(mergee) => {
+                        merge_maps_with(mergee, map, |mergee, member, occurences| *mergee.entry(member).or_insert(0) += occurences);
+                    },
+                }
+            }
+        }
         Self {
             num_tokens: tokens_map.len(),
             members_tokens_map,
